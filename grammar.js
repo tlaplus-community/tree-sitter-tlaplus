15070317cc21a98df72d15d8ba0f7a2a874bdd89
Andrew Helwer <ahelwer@protonmail.com>
2021-03-22 14:17:39 -0400

    Finished transcribing formal spec

--- grammar.js
+++ grammar.js
@@ -1,7 +1,9 @@
+// A sequence of one or more comma-separated strings matching the rule
 function commaList1(rule) {
   return seq(rule, repeat(seq(',', rule)))
 }
 
+// A sequence of zero or more comma-separated strings matching the rule
 function commaList(rule) {
   return optional(commaList1(rule))
 }
@@ -149,7 +151,7 @@ module.exports = grammar({
         seq($.identifier, $.postfix_op)
       ),
       '==',
-      $.expression
+      $._expression
     ),
 
     // Named operator left-hand-side, with or without parameters
@@ -170,117 +172,234 @@ module.exports = grammar({
       commaList1($.quantifier_bound),
       ']',
       '==',
-      $.expression
+      $._expression
     ),
 
     // x, y, z \in S
     quantifier_bound: $ => seq(
       choice($.identifier_or_tuple, commaList1($.identifier)),
       '\\in',
-      $.expression
+      $._expression
     ),
 
-    // A recursive parse rule for TLA+ expressions
-    _expression: $ => choice(
+    // INSTANCE ModuleName WITH x <- y, w <- z
+    instance: $ => seq(
+      'INSTANCE',
+      $.name,
+      optional(seq('WITH'), commaList1($.substitution))
+    ),
+
+    // x <- y, w <- z
+    substitution: $ => seq(
+      choice($.identifier, $.prefix_op, $.infix_op, $.postfix_op),
+      '<-',
+      $.argument
+    ),
+
+    argument: $ => choice(
+      $._expression,
+      $.general_prefix_op,
+      $.general_infix_op,
+      $.general_postfix_op
+    ),
+
+    // Foo(x, y)!Bar(w, z)!...
+    instance_prefix: $ => repeat(
       $.identifier,
-      $._set_definition,
-      $._set_operator,
-      $._primitive_literal,
+      optional(seq('(', commaList1($._expression), ')')),
+      '!'
     ),
 
-    // Ways of defining a new set.
-    _set_definition: $ => choice(
+    // Foo!bar
+    general_identifier: $ => seq($.instance_prefix, $.identifier),
+
+    // Foo!\neg
+    general_prefix_op: $ => seq($.instance_prefix, $.prefix_op),
+
+    // Foo!+
+    general_infix_op: $ => seq($.instance_prefix, $.infix_op),
+
+    // Foo!^#
+    general_postfix_op: $ => seq($.instance_prefix, $.postfix_op),
+
+    // M == INSTANCE ModuleName
+    module_definition: $ => seq(
+      $.non_fix_lhs,
+      '==',
+      $.instance
+    ),
+
+    // ASSUME C \in Nat
+    assumption: $ => seq(
+      choice('ASSUME', 'ASSUMPTION', 'AXIOM'),
+      $._expression
+    ),
+
+    // THEOREM Spec => []Safety
+    theorem: $ => seq('THEOREM', $._expression),
+
+    // Anything that evaluates to a value
+    _expression: $ => choice(
+      $.general_identifier,
+      seq($.general_identifier, '(', commaList1($.argument), ')'),
+      seq($.general_prefix_op, $._expression),
+      seq($._expression, $.general_infix_op, $._expression),
+      seq($._expression, $.general_postfix_op),
+      seq('(', $._expression, ')'),
+      seq(choice('\\A', '\\E'), commaList1($.quantifier_bound), ':', $._expression),
+      seq(choice('\\A', '\\E', '\\AA', '\\EE'), commaList1($.identifier), ':', $._expression),
+      $.choose,
       $.finite_set_literal,
-      $.range_set_literal,
+      $.set_filter,
       $.set_map,
-      $.set_filter
+      $.function_evaluation,
+      $.function_definition,
+      $.set_of_functions,
+      $.record_definition,
+      $.set_of_records,
+      $.except,
+      $.prev_func_val,
+      $.tuple_literal,
+      $.cross_product,
+      $.step_expression_or_stutter,
+      $.step_expression_no_stutter,
+      $.fairness,
+      $.if_then_else,
+      $.case,
+      $.let_in,
+      $.conj,
+      $.disj,
+      $.number,
+      $.string
+    ),
+
+    // CHOOSE r \in Real : r >= 0
+    choose: $ => seq(
+      'CHOOSE',
+      $.identifier_or_tuple,
+      optional(seq('\\in', $._expression)),
+      ':',
+      $._expression
     ),
 
     // {1, 2, 3, 4, 5}
-    finite_set_literal: $ => seq(
-      '{',
-      optional(
-        seq(
-          repeat(seq($._expression, ',')),
-          $._expression
-        )
-      ),
-      '}'
-    ),
+    finite_set_literal: $ => seq('{', commaList($._expression), '}'),
 
-    // 0 .. 4
-    range_set_literal: $ => prec.left(
-      seq(
-        $._expression,
-        '..',
-        $._expression
-      )
+    // { x \in S : P(x) }
+    set_filter: $ => seq(
+      '{', $.identifier_or_tuple, '\\in', $._expression, ':', $._expression, '}'
     ),
 
     // { f[x, y] : x, y \in S }
     set_map: $ => seq(
-      '{',
+      '{', $._expression, ':', commaList1($.quantifier_bound), '}'
+    ),
+
+    // f[5]
+    function_evaluation: $ => seq(
+      $._expression, '[', commaList1($._expression), ']'
+    ),
+
+    // [n \in Nat |-> 2*n]
+    function_definition: $ => seq(
+      '[', commaList1($.quantifier_bound), '|->', $._expression, ']'
+    ),
+
+    // [Nat -> Nat]
+    set_of_functions: $ => seq(
+      '[', $._expression, '->', $._expression, ']'
+    ),
+
+    // [foo |-> 0, bar |-> 1]
+    record_definition: $ => seq(
+      '[', commaList1(seq($.name, '|->', $._expression)), ']'
+    ),
+
+    // [foo : {0, 1}, bar : {0, 1}]
+    set_of_records: $ => seq(
+      '[', commaList1(seq($.name, ':', $._expression)), ']'
+    ),
+
+    // [f EXCEPT !.foo[bar].baz = 4, !.bar = 3]
+    except: $ => seq(
+      '[',
       $._expression,
-      ':',
-      $.multiple_named_elements_in,
-      '}'
+      'EXCEPT',
+      commaList1(
+        seq(
+          '!',
+          repeat1(
+            choice(
+              seq('.', $.name),
+              seq('[', commaList1($._expression), ']')
+            )
+          ),
+          '=',
+          $._expression
+        )
+      ),
+      ']'
     ),
 
-    // { x \in S : P(x) }
-    set_filter: $ => seq(
-      '{',
-      $.single_named_element_in,
-      ':',
-      $._expression,
-      '}'
+    // [f EXCEPT ![2] = @ + 1]
+    prev_func_val: $ => '@',
+
+    // <<1,2,3,4,5>>, <<>>
+    tuple_literal: $ => seq('<<', commaList($._expression), '>>'),
+
+    // S \X T \X P
+    cross_product: $ => seq(
+      $._expression, repeat1(choice('\\X', '\\times'), $._expression)
     ),
 
-    // Operators on sets
-    _set_operator: $ => choice(
-      $.set_membership,
-      $.set_not_membership,
-      $.set_union,
-      $.set_intersection,
-      $.set_subset,
-      $.set_difference,
-      $.set_powerset,
-      $.set_flatten
+    // [x ' > x]_<<x>>
+    step_expression_or_stutter: $ => seq(
+      '[', $._expression, ']_', $._expression
     ),
 
-    set_membership: $ => prec.left(seq($._expression, '\\in', $._expression)),
-    set_not_membership: $ => prec.left(seq($._expression, '\\notin', $._expression)),
-    set_union: $ => prec.left(seq($._expression, '\\cup', $._expression)),
-    set_intersection: $ => prec.left(seq($._expression, '\\cap', $._expression)),
-    set_subset: $ => prec.left(seq($._expression, '\\subseteq', $._expression)),
-    set_difference: $ => prec.left(seq($._expression, '\\', $._expression)),
-    set_powerset: $ => prec.left(seq('SUBSET', $._expression)),
-    set_flatten: $ => prec.left(seq('UNION', $._expression)),
-
-    // A single literal hardcoded value
-    _primitive_literal: $ => choice(
-      $.boolean_literal,
-      $.integer_literal,
-      $.real_literal,
-      $.string_literal
+    // <<x' > x>>_<<x>>
+    step_expression_no_stutter: $ => seq(
+      '<<', $._expression, '>>_', $._expression
     ),
+
+    // WF_vars(ActionName)
+    fairness: $ => seq(
+      choice('WF_', 'SF_'), $._expression, '(', $._expression, ')'
+    ),
+
+    // IF a > b THEN a ELSE b
+    if_then_else: $ => seq(
+      'IF', $._expression, 'THEN', $._expression, 'ELSE', $._expression
+    ),
+
+    // CASE x = 1 -> "1" [] x = 2 -> "2" [] OTHER -> "3"
+    case: $ => seq(
+      'CASE', $._expression, '->', $._expression,
+      repeat('[]', $._expression, '->', $._expression),
+      optional('[]', 'OTHER', '->', $._expression)
+    ),
+
+    // LET x == 5 IN 2*x
+    let_in: $ => seq(
+      'LET',
+      repeat1(
+        choice(
+          $.operator_definition,
+          $.function_definition,
+          $.module_definition
+        )
+      ),
+      'IN',
+      $._expression
+    ),
+
+    // x /\ y
+    conj: $ => repeat1(seq('/\\', $._expression)),
+
+    // x \/ y
+    disj: $ => repeat1(seq('\\/', $._expression)),
 
     // TRUE, FALSE
     boolean_literal: $ => choice('TRUE', 'FALSE'),
-
-    // x \in S
-    single_named_element_in: $ => seq(
-      $.identifier,
-      '\\in',
-      $._expression
-    ),
-
-    // x, y, z \in S
-    multiple_named_elements_in: $ => seq(
-      repeat(seq($.identifier, ',')),
-      $.identifier,
-      '\\in',
-      $._expression
-    ),
-
   }
 });
