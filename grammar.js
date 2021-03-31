3d31704aa2a103d55589fad3329cb4653949a50e
Andrew Helwer <ahelwer@protonmail.com>
2021-03-31 19:40:32 -0400

    Encoded infix operator precedence and unicode representation

--- grammar.js
+++ grammar.js
@@ -21,8 +21,15 @@ module.exports = grammar({
       'CASE',         'EXTENDS',    'SF_',        'WF_',
       'CHOOSE',       'IF',         'SUBSET',     'WITH',
       'CONSTANT',     'IN',         'THEN',
-      'CONSTANTS',    'INSTANCE',   'THEOREM',
-      'DOMAIN',       'LET',        'UNCHANGED'
+      'CONSTANTS',    'INSTANCE',   'THEOREM',    'COROLLARY',
+      'DOMAIN',       'LET',        'UNCHANGED',
+      'BY',           'HAVE',       'QED',        'TAKE',
+      'DEF',          'HIDE',       'RECURSIVE',  'USE',
+      'DEFINE',       'PROOF',      'WITNESS',    'PICK',
+      'DEFS',         'PROVE',      'SUFFICES',   'NEW',
+      'LAMBDA',       'STATE',      'ACTION',     'TEMPORAL',
+      'OBVIOUS',      'OMITTED',    'LEMMA',      'PROPOSITION',
+      'ONLY'
     ),
 
     // Name (module, definition, etc.)
@@ -48,14 +55,26 @@ module.exports = grammar({
     // "foobar", "", etc.
     string: $ => /\".*\"/,
 
-    // Prefix operators
+    // All prefix operators
     prefix_op: $ => choice(
-      '-',  '\\lnot', '[]', 'DOMAIN', 'ENABLED',  'UNION',
-      '~',  '\\neg',  '<>', 'SUBSET', 'UNCHANGED'
+      $.prefix_op_lnot,       $.prefix_op_union,    $.prefix_op_subset,
+      $.prefix_op_domain,     $.prefix_op_negative, $.prefix_op_enabled,
+      $.prefix_op_unchanged,  $.prefix_op_always,   $.prefix_op_eventually
     ),
 
+    // Prefix operators are given highest value in precedence range.
+    prefix_op_lnot:       $ => $.prec(4, choice('\\lnot', '\\neg', '~', '¬')),
+    prefix_op_union:      $ => $.prec(8, 'UNION'),
+    prefix_op_subset:     $ => $.prec(8, 'SUBSET'),
+    prefix_op_domain:     $ => $.prec(9, 'DOMAIN'),
+    prefix_op_negative:   $ => $.prec(12, '-'),
+    prefix_op_enabled:    $ => $.prec(15, 'ENABLED'),
+    prefix_op_unchanged:  $ => $.prec(15, 'UNCHANGED'),
+    prefix_op_always:     $ => $.prec(15, choice('[]', '□')),
+    prefix_op_eventually: $ => $.prec(15, choice('<>', '⋄')),
+
     // Infix operators
-    infix_op: $ => choice(
+    old_infix_op: $ => choice(
       '!!',   '#',    '##',   '$',    '$$',   '%',    '%%',
       '&',    '&&',   '(+)',  '(-)',  '(.)',  '(/)',  '(\\X)',
       '*',    '**',   '+',    '++',   '-',    '-+->', '--',
@@ -63,7 +82,7 @@ module.exports = grammar({
       '::=',  ':=',   ':>',   '<',    '<:',   '<=>',  '=',
       '=<',   '=>',   '=|',   '>',    '>=',   '?',    '??',
       '@@',   '\\',   '\\/',  '^',    '^^',   '|',    '|-',
-      '|=',   '||',   '~>',   '.',
+      '|=',   '||',   '~>',   '.',    '<=',
       '\\approx',   '\\geq',        '\\oslash',     '\\sqsupseteq',
       '\\asymp',    '\\gg',         '\\otimes',     '\\star',
       '\\bigcirc',  '\\in',         '\\prec',       '\\subset',
@@ -75,8 +94,128 @@ module.exports = grammar({
       '\\cup',      '\\o',          '\\sqcup',      '\\union',
       '\\div',      '\\odot',       '\\sqsubset',   '\\uplus',
       '\\doteq',    '\\ominus',     '\\sqsubseteq', '\\wr',
-      '\\equiv',    '\\oplus',      '\\sqsupset'
+      '\\equiv',    '\\oplus',      '\\sqsupset',   '\\notin',
     ),
+
+    infix_op: $ => choice(
+      $.infix_op_implies,     $.infix_op_plus_arrow,      $.infix_op_equiv,
+      $.infix_op_iff,         $.infix_op_leads_to,        $.infix_op_land,
+      $.infix_op_lor,         $.infix_op_assign,          $.infix_op_bnf_rule,
+      $.infix_op_eq,          $.infix_op_neq,             $.infix_op_lt,
+      $.infix_op_gt,          $.infix_op_leq,             $.infix_op_geq,
+      $.infix_op_approx,      $.infix_op_rs_ttile,        $.infix_op_rd_ttile,
+      $.infix_op_ls_ttile,    $.infix_op_ld_ttile,        $.infix_op_asymp,
+      $.infix_op_cong,        $.infix_op_doteq,           $.infix_op_gg,
+      $.infix_op_ll,          $.infix_op_in,              $.infix_op_notin,
+      $.infix_op_prec,        $.infix_op_succ,            $.infix_op_preceq,
+      $.infix_op_succeq,      $.infix_op_sim,             $.infix_op_simeq,
+      $.infix_op_sqsubset,    $.infix_op_sqsupset,        $.infix_op_sqsubseteq,
+      $.infix_op_sqsupseteq,  $.infix_op_subset,          $.infix_op_supset,
+      $.infix_op_subseteq,    $.infix_op_supseteq,        $.infix_op_compose,
+      $.infix_op_map_to,      $.infix_op_map_from,        $.infix_op_setminus,
+      $.infix_op_cap,         $.infix_op_cup,             $.infix_op_2dots,
+      $.infix_op_3dots,       $.infix_op_plus,            $.infix_op_plusplus,
+      $.infix_op_oplus,       $.infix_op_ominus,          $.infix_op_mod,
+      $.infix_op_modmod,      $.infix_op_vert,            $.infix_op_vertvert,
+      $.infix_op_minus,       $.infix_op_minusminus,      $.infix_op_amp,
+      $.infix_op_ampamp,      $.infix_op_odot,            $.infix_op_oslash,
+      $.infix_op_otimes,      $.infix_op_mul,             $.infix_op_mulmul,
+      $.infix_op_slash,       $.infix_op_slashslash,      $.infix_op_bigcirc,
+      $.infix_op_bullet,      $.infix_op_div,             $.infix_op_circ,
+      $.infix_op_star,        $.infix_op_excl,            $.infix_op_hashhash,
+      $.infix_op_dol,         $.infix_op_doldol,          $.infix_op_qq,
+      $.infix_op_sqcap,       $.infix_op_sqcup,           $.infix_op_uplus,
+      $.infix_op_wr,          $.infix_op_cdot,            $.infix_op_pow,
+      $.infix_op_powpow,      $.infix_op_rfield
+    ),
+
+    infix_op_implies:     $ => prec(1, choice('=>', '⟹')),
+    infix_op_plus_arrow:  $ => prec(2, choice('-+->', '⇸', '⍆', '⥅')),
+    infix_op_equiv:       $ => prec(2, choice('\\equiv', '≡')),
+    infix_op_iff:         $ => prec(2, choice('<=>', '⟺')),
+    infix_op_leads_to:    $ => prec(2, choice('~>', '⇝')),
+    infix_op_land:        $ => prec.left(3, choice('/\\', '\\land', '∧')),
+    infix_op_lor:         $ => prec.left(3, choice('\\/', '\\lor', '∨')),
+    infix_op_assign:      $ => prec(5, ':='),
+    infix_op_bnf_rule:    $ => prec(5, '::='),
+    infix_op_eq:          $ => prec(5, '='),
+    infix_op_neq:         $ => prec(5, choice('/=', '#', '≠')),
+    infix_op_lt:          $ => prec(5, '<'),
+    infix_op_gt:          $ => prec(5, '>'),
+    infix_op_leq:         $ => prec(5, choice('<=', '\\leq', '≤')),
+    infix_op_geq:         $ => prec(5, choice('>=', '\\geq', '≥')),
+    infix_op_approx:      $ => prec(5, choice('\\approx', '≈')),
+    infix_op_rs_ttile:    $ => prec(5, choice('|-', '⊢')),
+    infix_op_rd_ttile:    $ => prec(5, choice('|=', '⊨')),
+    infix_op_ls_ttile:    $ => prec(5, choice('-|', '⊣')),
+    infix_op_ld_ttile:    $ => prec(5, choice('=|', '⫤')),
+    infix_op_asymp:       $ => prec(5, choice('\\asymp', '≍')),
+    infix_op_cong:        $ => prec(5, choice('\\cong', '≅')),
+    infix_op_doteq:       $ => prec(5, choice('\\doteq', '≐')),
+    infix_op_gg:          $ => prec(5, choice('\\gg', '≫')),
+    infix_op_ll:          $ => prec(5, choice('\\ll', '≪')),
+    infix_op_in:          $ => prec(5, choice('\\in', '∈')),
+    infix_op_notin:       $ => prec(5, choice('\\notin', '∉')),
+    infix_op_prec:        $ => prec(5, choice('\\prec', '≺')),
+    infix_op_succ:        $ => prec(5, choice('\\succ', '≻')),
+    infix_op_preceq:      $ => prec(5, choice('\\preceq', '⪯')),
+    infix_op_succeq:      $ => prec(5, choice('\\succeq', '⪰')),
+    infix_op_propto:      $ => prec(5, choice('\\propto', '∝')),
+    infix_op_sim:         $ => prec(5, choice('\\sim', '∼')),
+    infix_op_simeq:       $ => prec(5, choice('\\simeq', '≃')),
+    infix_op_sqsubset:    $ => prec(5, choice('\\sqsubset', '⊏')),
+    infix_op_sqsupset:    $ => prec(5, choice('\\sqsupset', '⊐')),
+    infix_op_sqsubseteq:  $ => prec(5, choice('\\sqsubseteq', '⊑')),
+    infix_op_sqsupseteq:  $ => prec(5, choice('\\sqsupseteq', '⊒')),
+    infix_op_subset:      $ => prec(5, choice('\\subset', '⊂')),
+    infix_op_supset:      $ => prec(5, choice('\\supset', '⊃')),
+    infix_op_subseteq:    $ => prec(5, choice('\\subseteq', '⊆')),
+    infix_op_supseteq:    $ => prec(5, choice('\\supseteq', '⊇')),
+    infix_op_compose:     $ => prec.left(6, '@@'),
+    infix_op_map_to:      $ => prec(7, ':>'),
+    infix_op_map_from:    $ => prec(7, '<:'),
+    infix_op_setminus:    $ => prec(8, '\\'),
+    infix_op_cap:         $ => prec.left(8, choice('\\cap', '∩')),
+    infix_op_cup:         $ => prec.left(8, choice('\\cup', '∪')),
+    infix_op_2dots:       $ => prec(9, '..'),
+    infix_op_3dots:       $ => prec(9, choice('...', '…')),
+    infix_op_plus:        $ => prec.left(10, '+'),
+    infix_op_plusplus:    $ => prec.left(10, '++'),
+    infix_op_oplus:       $ => prec.left(10, choice('\\oplus', '⊕')),
+    infix_op_ominus:      $ => prec.left(11, choice('\\ominus', '⊖')),
+    infix_op_mod:         $ => prec(11, '%'),
+    infix_op_modmod:      $ => prec.left(11, '%%'),
+    infix_op_vert:        $ => prec.left(11, '|'),
+    infix_op_vertvert:    $ => prec.left(11, choice('||', '‖')),
+    infix_op_minus:       $ => prec.left(11, '-'),
+    infix_op_minusminus:  $ => prec.left(11, '--'),
+    infix_op_amp:         $ => prec.left(13, '&'),
+    infix_op_ampamp:      $ => prec.left(13, '&&'),
+    infix_op_odot:        $ => prec.left(13, choice('\\odot', '⊙')),
+    infix_op_oslash:      $ => prec(13, choice('\\oslash', '⊘')),
+    infix_op_otimes:      $ => prec.left(13, choice('\\otimes', '⊗')),
+    infix_op_mul:         $ => prec.left(13, '*'),
+    infix_op_mulmul:      $ => prec.left(13, '**'),
+    infix_op_slash:       $ => prec(13, '/'),
+    infix_op_slashslash:  $ => prec(13, '//'),
+    infix_op_bigcirc:     $ => prec.left(13, choice('\\bigcirc', '◯')),
+    infix_op_bullet:      $ => prec.left(13, choice('\\bullet', '●')),
+    infix_op_div:         $ => prec(13, choice('\\div', '÷')),
+    infix_op_circ:        $ => prec.left(13, choice('\\circ', '∘')),
+    infix_op_star:        $ => prec.left(13, choice('\\star', '⋆')),
+    infix_op_excl:        $ => prec(13, choice('!!', '‼')),
+    infix_op_hashhash:    $ => prec.left(13, '##'),
+    infix_op_dol:         $ => prec.left(13, '$'),
+    infix_op_doldol:      $ => prec.left(13, '$$'),
+    infix_op_qq:          $ => prec.left(13, '??'),
+    infix_op_sqcap:       $ => prec.left(13, choice('\\sqcap', '⊓')),
+    infix_op_sqcup:       $ => prec.left(13, choice('\\sqcup', '⊔')),
+    infix_op_uplus:       $ => prec.left(13, choice('\\uplus', '⊎')),
+    infix_op_wr:          $ => prec(14, choice('\\wr', '≀')),
+    infix_op_cdot:        $ => prec.left(14, choice('\\cdot', '⋅')),
+    infix_op_pow:         $ => prec(14, '^'),
+    infix_op_powpow:      $ => prec(14, '^^'),
+    infix_op_rfield:      $ => prec.left(17, '.'),
 
     // Postfix operators
     postfix_op: $ => choice('^+', '^*', '^#', '\''),
@@ -104,6 +243,8 @@ module.exports = grammar({
     unit: $ => choice(
         $.variable_declaration,
         $.constant_declaration,
+        $.recurisive_operator_declaration,
+        //$.use_or_hide
         seq(optional("LOCAL"), $.operator_definition),
         seq(optional("LOCAL"), $.function_definition),
         seq(optional("LOCAL"), $.instance),
@@ -124,6 +265,11 @@ module.exports = grammar({
     constant_declaration: $ => seq(
         choice('CONSTANT', 'CONSTANTS'),
         commaList1($.operator_declaration)
+    ),
+
+    // RECURSIVE op(_, _)
+    recurisive_operator_declaration: $ => seq(
+      'RECURSIVE', commaList1($.operator_declaration)
     ),
 
     // Operator declaration (not definition)
@@ -316,6 +462,7 @@ module.exports = grammar({
       //$.disj,
       $.string,
       $.number,
+      $.boolean
     ),
 
     set_membership: $ => seq($.expression, '\\in', $.expression),
@@ -477,7 +624,7 @@ module.exports = grammar({
     // x \/ y
     disj: $ => repeat1(seq('\\/', $.expression)),
 
-    // TRUE, FALSE
-    boolean_literal: $ => choice('TRUE', 'FALSE'),
+    // TRUE, FALSE, BOOLEAN
+    boolean: $ => choice('TRUE', 'FALSE', 'BOOLEAN'),
   }
 });
