7018d40390aa55d1d0286a5186a7b5813e50c50d
Andrew Helwer <ahelwer@protonmail.com>
2021-04-20 14:28:31 -0400

    Whitespace parsing

--- grammar.js
+++ grammar.js
@@ -1,50 +1,35 @@
-function commaList1(rule) {
-  return seq(rule, repeat(seq(',', rule)))
-}
-
-function commaList(rule) {
-  return optional(commaList1(rule))
-}
+const NL = /[\n\r]+/
+const WS = /[\t ]+/
 
 module.exports = grammar({
-  name: 'tlaplus',
+    name: 'test',
 
-  conflicts: $ => [
-    //[$.set_membership, $.set_membership],
-    //[$.cross_product, $.cross_product],
-    //[$.cross_product, $.set_union]
-  ],
+    conflicts: $ => [
+      [$.list],
+    ],
 
-  inline: $ => [
-    $.expression
-  ],
+    rules: {
 
-  rules: {
-    source_file: $ => $.expression,
+        source_file: $ => repeat(
+            $._stmt,
+        ),
 
-    expression: $ => choice(
-      prec(1, $.identifier),
-      $.value,
-      //$.cross_product,
-      $.parenthesis,
-      $.set_union,
-      $.subseteq,
-    ),
+        _stmt: $ => seq(
+            $.list, optional(WS), ';'
+        ),
 
-    //set_union: $ => prec.dynamic(-2, prec.left(8, seq($.expression, '\\cup', $.expression))),
-    set_union: $ => prec.left(8, seq($.expression, '\\cup', $.expression)),
+        list: $ => seq(
+            $.sel,
+            repeat(seq(WS, $.sel))
+        ),
 
-    subseteq: $ => prec(5, seq($.expression, '\\subseteq', $.expression)),
+        sel: $ => seq(
+            optional('.'),
+            $.id,
+            repeat(seq('.', $.id))
+        ),
 
-    identifier: $ => /\w*[A-Za-z]\w*/,
+        id: $ => /[a-zA-Z]+/
 
-    parenthesis: $ => seq('(', $.expression, ')'),
-
-    value: $ => /\d+/,
-
-    cross_product: $ => prec.dynamic(-1, seq(
-      repeat1(seq($.expression, '\\X')),
-      $.expression
-    )),
-  }
+    }
 });
