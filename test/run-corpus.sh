c7fdf835831e036be842c261a594c1c06b2a25f1
Andrew Helwer <2n8rn1w1f@mozmail.com>
2023-01-19 12:07:31 -0500

    Correctly get CPU count for macos

--- test/run-corpus.sh
+++ test/run-corpus.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
 specs=$(find "test/examples" -name "*.tla")
-failures=$(echo "$specs" | xargs -P $(nproc) -I {} ./node_modules/.bin/tree-sitter parse -q {})
+ncpu=$(nproc 2> /dev/null || sysctl -n hw.ncpu 2> /dev/null || 1)
+failures=$(echo "$specs" | xargs -P $ncpu -I {} ./node_modules/.bin/tree-sitter parse -q {})
 if test -z "$failures"; then
   exit 0
 else
   echo $failures
-  exit -1
+  exit 1
 fi
 
