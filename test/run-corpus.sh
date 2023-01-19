bd6a368cf21d590c3079822e074523c2177a3276
Andrew Helwer <2n8rn1w1f@mozmail.com>
2023-01-19 12:16:46 -0500

    Don't paralellize on macos, it deadlocks

--- test/run-corpus.sh
+++ test/run-corpus.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 specs=$(find "test/examples" -name "*.tla")
-ncpu=$(nproc 2> /dev/null || sysctl -n hw.ncpu 2> /dev/null || 1)
+ncpu=$(nproc 2> /dev/null || 1)
+echo "$ncpu"
 failures=$(echo "$specs" | xargs -P $ncpu -I {} ./node_modules/.bin/tree-sitter parse -q {})
 if test -z "$failures"; then
   exit 0
