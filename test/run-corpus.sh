c74bf6899338061a5b9dbf6db9d233fde0f2eda0
Jones Martins <15959626+jonesmartins@users.noreply.github.com>
2023-01-18 21:30:17 -0300

    Fix 'No such file' error when running run-corpus.sh

--- test/run-corpus.sh
+++ test/run-corpus.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 find "test/examples/external/specifications" -name "*.tla" ! -iname "Reals.tla" ! -iname "Naturals.tla" |
-    xargs -P $(nproc) -I {} "npx tree-sitter parse -q {}"
+    xargs -P $(nproc) -I {} npx tree-sitter parse -q {}
