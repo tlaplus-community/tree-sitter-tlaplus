7f2a0cca2c9192b2b65bb82ca614f8d5b5aa6b13
Andrew Helwer <2n8rn1w1f@mozmail.com>
2023-01-24 12:42:13 -0500

    Try again with parallel

--- test/run-corpus.ps1
+++ test/run-corpus.ps1
@@ -1,5 +1,7 @@
 $specs = Get-ChildItem -Path .\test\examples -Filter "*.tla" -Recurse
-$failures = $specs | ForEach-Object -Parallel -ThrottleLimit 2 {.\node_modules\.bin\tree-sitter parse -q $_}
+$cpuCount = (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
+$cpuCount
+$failures = $specs | ForEach-Object -Parallel {.\node_modules\.bin\tree-sitter parse -q $_} -ThrottleLimit $cpuCount
 $failures
 exit $failures.length
 
