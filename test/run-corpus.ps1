56f7e225b53d2af1ba56e683fce9f1a6f90d1e19
Andrew Helwer <2n8rn1w1f@mozmail.com>
2023-01-24 12:37:24 -0500

    Try parallelizing windows corpus script

--- test/run-corpus.ps1
+++ test/run-corpus.ps1
@@ -1,5 +1,5 @@
 $specs = Get-ChildItem -Path .\test\examples -Filter "*.tla" -Recurse
-$failures = $specs |% {.\node_modules\.bin\tree-sitter parse -q $_}
+$failures = $specs | ForEach-Object -Parallel -ThrottleLimit 2 {.\node_modules\.bin\tree-sitter parse -q $_}
 $failures
 exit $failures.length
 
