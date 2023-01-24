6fd16d8469c6898317423d61738d97e2b3f5caf7
Andrew Helwer <ahelwer@users.noreply.github.com>
2023-01-24 13:01:29 -0500

    Detect fair pcal algorithms (#77)
    Fixes #76

--- test/run-corpus.ps1
+++ test/run-corpus.ps1
@@ -1,5 +1,6 @@
 $specs = Get-ChildItem -Path .\test\examples -Filter "*.tla" -Recurse
-$failures = $specs |% {.\node_modules\.bin\tree-sitter parse -q $_}
+$cpuCount = (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
+$failures = $specs | ForEach-Object -Parallel {.\node_modules\.bin\tree-sitter parse -q $_} -ThrottleLimit $cpuCount
 $failures
 exit $failures.length
 
