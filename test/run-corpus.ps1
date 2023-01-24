37e12d50aec82ad954542447b30d453299211c1c
Andrew Helwer <2n8rn1w1f@mozmail.com>
2023-01-24 12:46:25 -0500

    Remove corecount output

--- test/run-corpus.ps1
+++ test/run-corpus.ps1
@@ -1,6 +1,5 @@
 $specs = Get-ChildItem -Path .\test\examples -Filter "*.tla" -Recurse
 $cpuCount = (Get-CimInstance -ClassName Win32_Processor).NumberOfLogicalProcessors
-$cpuCount
 $failures = $specs | ForEach-Object -Parallel {.\node_modules\.bin\tree-sitter parse -q $_} -ThrottleLimit $cpuCount
 $failures
 exit $failures.length
