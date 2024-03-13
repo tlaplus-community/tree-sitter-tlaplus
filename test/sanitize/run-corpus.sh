58e789bf31527a242ee21c63b57d49445fbf2b34
Andrew Helwer <2n8rn1w1f@mozmail.com>
2024-03-13 16:30:46 -0400

    Run corpus against sanitized build on linux
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

---
+++ test/sanitize/run-corpus.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+specs=$(find "test/examples" -name "*.tla")
+ncpu=$(($# > 0 ? $(command -v nproc > /dev/null && nproc || echo 1) : 1))
+echo "Concurrent parser count: $ncpu"
+failures=$(echo "$specs" | xargs -P $ncpu -I {} ./test/sanitize/out/parse-tlaplus {} -q)
+if test -z "$failures"; then
+  exit 0
+else
+  echo "$failures"
+  exit 1
+fi
+
