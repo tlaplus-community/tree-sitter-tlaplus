fdcb2c90cc17a456639bfd85217a721a13f18f32
Andrew Helwer <2n8rn1w1f@mozmail.com>
2024-03-13 16:54:00 -0400

    Trying again with sanitizer script
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- test/sanitize/run-corpus.sh
+++ test/sanitize/run-corpus.sh
@@ -1,13 +1,6 @@
 #! /bin/sh
-
-specs=$(find "test/examples" -name "*.tla")
-ncpu=$(($# > 0 ? $(command -v nproc > /dev/null && nproc || echo 1) : 1))
-echo "Concurrent parser count: $ncpu"
-failures=$(echo "$specs" | xargs -P $ncpu -I {} echo {} && ./test/sanitize/out/parse_tlaplus {} -q)
-if test -z "$failures"; then
-  exit 0
-else
-  echo "$failures"
-  exit 1
-fi
+find "test/examples" -type f -name "*.tla" | while IFS= read -r file; do
+    echo "$file"
+    ./test/sanitize/out/parse_tlaplus "$file" -q
+done
 
