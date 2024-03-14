8ee94abf2aee785a2cea5a4a1ab9cc802c46da64
Andrew Helwer <2n8rn1w1f@mozmail.com>
2024-03-13 21:44:46 -0400

    Fixed script
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- test/sanitize/run-corpus.sh
+++ test/sanitize/run-corpus.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
-SUCCESS=1
+EXITCODE=0
 find "test/examples" -type f -name "*.tla" | while IFS= read -r file; do
   echo "$file"
   ./test/sanitize/out/parse_tlaplus "$file" -q
   if [ $? -ne 0 ]; then
-    SUCCESS=0
+    echo "FAILURE: $?"
+    EXITCODE=1
+    ./test/sanitize/out/parse_tlaplus "$file"
   fi
 done
 exit $SUCCESS
