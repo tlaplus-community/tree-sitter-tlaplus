1e277595fa4c6e46fc89459eeef72b849f2c0b96
Andrew Helwer <2n8rn1w1f@mozmail.com>
2024-03-13 21:34:03 -0400

    Updated tree-sitter submodule
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- test/sanitize/run-corpus.sh
+++ test/sanitize/run-corpus.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
+SUCCESS=1
 find "test/examples" -type f -name "*.tla" | while IFS= read -r file; do
-    echo "$file"
-    ./test/sanitize/out/parse_tlaplus "$file" -q
+  echo "$file"
+  ./test/sanitize/out/parse_tlaplus "$file" -q
+  if [ $? -ne 0 ]; then
+    SUCCESS=0
+  fi
 done
+exit $SUCCESS
 
