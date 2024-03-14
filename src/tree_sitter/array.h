4be4bd39ba5fb285cf7ea7c2e18d6db8e1450b7c
Andrew Helwer <2n8rn1w1f@mozmail.com>
2024-03-13 21:21:44 -0400

    Reverted array.h changes
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- src/tree_sitter/array.h
+++ src/tree_sitter/array.h
@@ -66,12 +66,9 @@ extern "C" {
 /// Increase the array's size by `count` elements.
 /// New elements are zero-initialized.
 #define array_grow_by(self, count) \
-  do { \
-    if ((count) == 0) break; \
-    _array__grow((Array *)(self), count, array_elem_size(self)); \
-    memset((self)->contents + (self)->size, 0, (count) * array_elem_size(self)); \
-    (self)->size += (count); \
-  } while (0)
+  (_array__grow((Array *)(self), count, array_elem_size(self)), \
+   memset((self)->contents + (self)->size, 0, (count) * array_elem_size(self)), \
+   (self)->size += (count))
 
 /// Append all elements from one array to the end of another.
 #define array_push_all(self, other)                                       \
