22e7897f2e6c7dc817b6f027aed1301c1e93f327
Andrew Helwer <ahelwer@users.noreply.github.com>
2024-05-15 18:22:17 -0400

    Fix python consumer (#118)
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- shell.nix
+++ shell.nix
@@ -1,14 +1,22 @@
-let pkgs = import <nixpkgs> {};
-in pkgs.mkShell {
-  packages = with pkgs; [
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  unstable = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz";
+    sha256 = "0lisfh5b3dgcjb083nsxmffvzxzs6ir1jfsxfaf29gjpjnv7sm7f";
+  }) {};
+in
+
+pkgs.mkShell {
+  packages = [
     (pkgs.python3.withPackages (python-pkgs: [
       python-pkgs.setuptools
       python-pkgs.wheel
       python-pkgs.build
     ]))
-    (tree-sitter.override { webUISupport = true; })
-    emscripten
-    gcc
+    (unstable.tree-sitter.override { webUISupport = true; })
+    pkgs.emscripten
+    pkgs.gcc
   ];
 }
 
