9292ae2e5d2461eeadd6810ed740e547707e18d5
Andrew Helwer <2n8rn1w1f@mozmail.com>
2024-05-15 17:12:05 -0400

    Fix python consumer
    Signed-off-by: Andrew Helwer <2n8rn1w1f@mozmail.com>

--- test/consumers/python/main.py
+++ test/consumers/python/main.py
@@ -1,17 +1,16 @@
 import tree_sitter_tlaplus as tstla
 from tree_sitter import Language, Parser
 
-TLAPLUS_LANGUAGE = Language(tstla.language(), 'tlaplus')
-parser = Parser()
-parser.set_language(TLAPLUS_LANGUAGE)
+TLAPLUS_LANGUAGE = Language(tstla.language())
+parser = Parser(TLAPLUS_LANGUAGE)
 tree = parser.parse(bytes("""
 ---- MODULE Test ----
 op ≜ ∀ n ∈ ℕ : n ≥ 0
 ====
 """, "utf8"))
-print(tree.root_node.sexp())
+print(tree.root_node)
 
-query = TLAPLUS_LANGUAGE.query('(def_eq) @capture')
+query = TLAPLUS_LANGUAGE.query('(def_eq \"≜\") @capture')
 for node, capture_name in query.captures(tree.root_node):
     print(node)
 
