3ed77e4e1ae7ec3b8fd76a0850da0f21c1943427
Rebecca Valentine <beka@gothdyke.mom>
2024-09-27 18:31:34 -0700

    Updates test consumer tree-sitter version

--- test/consumers/rust/src/main.rs
+++ test/consumers/rust/src/main.rs
@@ -2,7 +2,9 @@ use tree_sitter::{Parser, Query, QueryCursor};
 
 fn main() {
     let mut parser = Parser::new();
-    parser.set_language(&tree_sitter_tlaplus::language()).expect("Error loading TLA+ grammar");
+    parser
+        .set_language(&tree_sitter_tlaplus::LANGUAGE.into())
+        .expect("Error loading TLA+ grammar");
     let source_code = r#"
         ---- MODULE Test ----
         op ≜ ∀ n ∈ ℕ : n ≥ 0
@@ -10,10 +12,13 @@ fn main() {
     let tree = parser.parse(source_code, None).unwrap();
     println!("{}", tree.root_node().to_sexp());
 
-    let query = Query::new(&tree_sitter_tlaplus::language(), "(def_eq \"≜\") @capture").unwrap();
+    let query = Query::new(
+        &tree_sitter_tlaplus::LANGUAGE.into(),
+        "(def_eq \"≜\") @capture",
+    )
+    .unwrap();
     let mut cursor = QueryCursor::new();
     for capture in cursor.matches(&query, tree.root_node(), "".as_bytes()) {
         println!("{:?}", capture);
     }
 }
-
